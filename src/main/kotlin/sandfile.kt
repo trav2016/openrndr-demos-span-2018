bbd1b5de54662bddf26e07904908b472d86c0db1
trav2016 <travis.l.sims@gmail.com>
2018-12-06 16:49:03 -0500

    Update sandfile.kt

--- src/main/kotlin/sandfile.kt
+++ src/main/kotlin/sandfile.kt
@@ -1,1 +1,27 @@
+import demos.DemoRunner
+import demos.MyDemo
+import org.openrndr.application
+import org.openrndr.configuration
+import org.openrndr.extensions.Debug3D
+import org.openrndr.extensions.FunctionDrawer
 
+
+class Sandbox : DemoRunner() {
+    override lateinit var camera: Debug3D
+
+    override fun setup() {
+        camera = Debug3D()
+        val demo = MyDemo(this)
+        extend(FunctionDrawer {
+            demo()
+        })
+    }
+}
+
+
+fun main(args: Array<String>) {
+    application(Sandbox(), configuration {
+        width = 1920
+        height = 1080
+    })
+}
